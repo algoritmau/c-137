346affc6a0b68b2c31978c234b58e11f67061e46
Mauricio Paternina <spaceinvadev@icloud.com>
2022-06-08 23:43:01 -0500

    set up data fetching and rendering

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,4 +1,10 @@
-const Character = () => <h3>Character</h3>
+const Character = (character) => (
+  <div>
+    <h3>{character.name}</h3>
+    <img src={character.image} alt={character.name} width='300' />
+    <p>{`Origin: ${character.origin ? character.origin.name : 'Unknown'}`}</p>
+  </div>
+)
 
 export default Character
 
--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,41 @@
+import { useEffect, useState } from 'react'
+
 import Character from './Character'
 
-const List = () => (
-  <div>
-    <h2>Characters</h2>
-    <Character />
-    <Character />
-  </div>
-)
+const List = () => {
+  const [loading, setLoading] = useState(true)
+  const [characters, setCharacters] = useState([])
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('https://rickandmortyapi.com/api/character')
+      const { results } = await data.json()
+
+      setCharacters(results)
+      setLoading(false)
+    }
+
+    fetchData()
+  }, [characters.length])
+
+  return (
+    <div>
+      <h2>Characters</h2>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        characters.map((character) => (
+          <Character
+            key={character.id}
+            name={character.name}
+            origin={character.origin}
+            image={character.image}
+          />
+        ))
+      )}
+    </div>
+  )
+}
 
 export default List
 
